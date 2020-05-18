870719c15b33620b60199746288bd0e522b8bc97
axel.candia <axelcandia2609@gmail.com>
2020-05-18 15:35:33 -0300

    Show forced creation in the metabox

--- Orders/Metabox.php
+++ Orders/Metabox.php
@@ -46,55 +46,53 @@ class Metabox
             'text_order_status_name' => __('Mark order as ready to be shipped', 'wc-moova'),
             'text_error' => __('There was an error, please try again', 'wc-moova')
         ]);
-        $shipping_methods = $order->get_shipping_methods();
-        if (empty($shipping_methods)) {
+        $shipping_method = Helper::getShippingMethod($order);
+        if (!$shipping_method) {
             return;
         }
-        $shipping_method = array_shift($shipping_methods);
-        if ($shipping_method->get_method_id() === 'moova') {
-            $config_status = Helper::get_option('status_processing');
-            $config_status = str_replace('wc-', '', $config_status);
-            if ($order->has_status($config_status) || $config_status === '0' || $shipping_method->get_meta('tracking_number')) {
-                $tracking_number = $shipping_method->get_meta('tracking_number');
-                if (!empty($tracking_number)) {
 
-                    // Tracking number
-                    preg_match('/([a-zA-Z0-9]+)-/', $tracking_number, $matches);
-                    $tracking_number = $matches[1];
-                    printf(__('The order has been processed, tracking number: <strong>%s</strong>', 'wc-moova'), $tracking_number);
+        $config_status = Helper::get_option('status_processing');
+        $config_status = str_replace('wc-', '', $config_status);
+        if ($order->has_status($config_status) || $config_status === '0' || $shipping_method->get_meta('tracking_number')) {
+            $tracking_number = $shipping_method->get_meta('tracking_number');
+            if (!empty($tracking_number)) {
 
-                    // Tracking URL
-                    if (Helper::get_option('environment') === 'prod') {
-                        $tracking_url = 'https://dashboard.moova.io/external?id=' . $tracking_number;
-                    } else {
-                        $tracking_url = 'https://dev.moova.io/external?id=' . $tracking_number;
-                    }
-                    echo '<a class="button-primary" style="display:block;margin:10px 0;" href="' . $tracking_url . '" target="_blank">' . __('Track order', 'wc-moova') . '</a>';
+                // Tracking number
+                preg_match('/([a-zA-Z0-9]+)-/', $tracking_number, $matches);
+                $tracking_number = $matches[1];
+                printf(__('The order has been processed, tracking number: <strong>%s</strong>', 'wc-moova'), $tracking_number);
 
-                    // Label URL
-                    $label_url = $shipping_method->get_meta('shipping_label');
-                    if (empty($label_url)) {
-                        echo '<a class="button-primary" style="display:block;margin:10px 0;" target="_blank" data-action="generate_order_shipping_label">' . __('Generate shipping label', 'wc-moova') . '</a>';
-                    } else {
-                        echo '<a class="button-primary" style="display:block;margin:10px 0;" target="_blank" href="' . $label_url . '">' . __('View shipping label', 'wc-moova') . '</a>';
-                    }
-
-                    // Update status
-                    $moova_sdk = new MoovaSdk();
-                    $moova_status = $moova_sdk->get_order_status($tracking_number);
-                    if (trim(strtoupper($moova_status)) === 'DRAFT') {
-                        echo '<a class="button-primary" style="display:block;margin:10px 0;" data-action="change_order_status" data-to-status="ready">' . __('Mark order as ready to be shipped', 'wc-moova') . '</a>';
-                    }
+                // Tracking URL
+                if (Helper::get_option('environment') === 'prod') {
+                    $tracking_url = 'https://dashboard.moova.io/external?id=' . $tracking_number;
                 } else {
-                    echo __('The order is not processed yet', 'wc-moova');
-                    if ($config_status === '0') {
-                        echo '<a class="button-primary" style="display:block;margin:10px 0;" target="_blank" data-action="process_order">' . __('Process order', 'wc-moova') . '</a>';
-                    }
+                    $tracking_url = 'https://dev.moova.io/external?id=' . $tracking_number;
+                }
+                echo '<a class="button-primary" style="display:block;margin:10px 0;" href="' . $tracking_url . '" target="_blank">' . __('Track order', 'wc-moova') . '</a>';
+
+                // Label URL
+                $label_url = $shipping_method->get_meta('shipping_label');
+                if (empty($label_url)) {
+                    echo '<a class="button-primary" style="display:block;margin:10px 0;" target="_blank" data-action="generate_order_shipping_label">' . __('Generate shipping label', 'wc-moova') . '</a>';
+                } else {
+                    echo '<a class="button-primary" style="display:block;margin:10px 0;" target="_blank" href="' . $label_url . '">' . __('View shipping label', 'wc-moova') . '</a>';
+                }
+
+                // Update status
+                $moova_sdk = new MoovaSdk();
+                $moova_status = $moova_sdk->get_order_status($tracking_number);
+                if (trim(strtoupper($moova_status)) === 'DRAFT') {
+                    echo '<a class="button-primary" style="display:block;margin:10px 0;" data-action="change_order_status" data-to-status="ready">' . __('Mark order as ready to be shipped', 'wc-moova') . '</a>';
                 }
             } else {
-                $statuses = wc_get_order_statuses();
-                printf(__('The order will be processed when its status is <strong>%s</strong>', 'wc-moova'), $statuses['wc-' . $config_status]);
+                echo __('The order is not processed yet', 'wc-moova');
+                if ($config_status === '0') {
+                    echo '<a class="button-primary" style="display:block;margin:10px 0;" target="_blank" data-action="process_order">' . __('Process order', 'wc-moova') . '</a>';
+                }
             }
+        } else {
+            $statuses = wc_get_order_statuses();
+            printf(__('The order will be processed when its status is <strong>%s</strong>', 'wc-moova'), $statuses['wc-' . $config_status]);
         }
     }
 }
