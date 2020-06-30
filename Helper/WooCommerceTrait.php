bb291e2efef96855e282b0d2b86593321c36f86e
axel.candia <axelcandia2609@gmail.com>
2020-06-30 12:49:44 -0300

    Create shipping with score included

--- Helper/WooCommerceTrait.php
+++ Helper/WooCommerceTrait.php
@@ -220,7 +220,7 @@ trait WooCommerceTrait
         }
 
         //Now let's work on the first line
-        preg_match('/(^\d*[\D]*)(\d+)(.*)/i', $shipping_line_1, $res);
+        preg_match('/(^\d*[\D]*)([^\s]+)(.*)/i', $shipping_line_1, $res);
         $line1 = $res;
 
         if ((isset($line1[1]) && !empty($line1[1]) && $line1[1] !== " ") && !empty($line1)) {
