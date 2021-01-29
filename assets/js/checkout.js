d2362b6adceef698e0d4a15024a995ec183c9338
axel.candia <axelcandia2609@gmail.com>
2021-01-29 10:20:46 -0300

    Improve checkout

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,29 +1,63 @@
 "use strict";
 
 function initMap() {
+  //Init
   const $ = jQuery;
-  const mapExists = true;
-    $("<div id='moova-map' style='height: 400px;' ></div>").insertBefore(".woocommerce-additional-fields__field-wrapper");
-    const map = new google.maps.Map(document.getElementById("moova-map"), {
-      center: { lat: -33.8688, lng: 151.2195 },
-      zoom: 13,
-    });
-  
-  const fieldToListen = true ? 'billing' : 'shipping';
-  const input = document.getElementById("billing_address_1");
-  const autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.bindTo("bounds", map);
-  // Set the data fields to return when the user selects a place.
-  autocomplete.setFields(["address_components", "geometry", "icon", "name"]);
+  $("<div id='moova-map' style='height: 400px;' ></div>").insertBefore(".woocommerce-additional-fields__field-wrapper");
+  const map = new google.maps.Map(document.getElementById("moova-map"), {
+    center: { lat: -33.8688, lng: 151.2195 },
+    zoom: 13,
+  });
+
   const marker = new google.maps.Marker({
     map,
     anchorPoint: new google.maps.Point(0, -29),
   });
 
-  autocomplete.addListener("place_changed", () => { 
-    marker.setVisible(false);
-    const place = autocomplete.getPlace();
+  //Set autocomplete
+  const types = ["billing","shipping"];
+  for (let type of types) {
+    let autocomplete = setAutocomplete(map, type); 
+    let place = new Place(type, map, marker, autocomplete);
+    autocomplete.addListener("place_changed", () => { place.changed() });
+  }
+  
+  google.maps.event.addListener(map, 'click', function(event) {
+    placeMarker(event.latLng, marker);
+  });
+}
 
+function setAutocomplete(map, type) {
+  const input = document.getElementById(type+"_address_1");
+  const autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo("bounds", map);
+  // Set the data fields to return when the user selects a place.
+  autocomplete.setFields(["address_components", "geometry", "icon", "name"]);
+  return autocomplete;
+}
+
+function placeMarker(location, marker) {
+    document.getElementById('billing_moova_lat').value = location.lat();
+    document.getElementById('billing_moova_lng').value = location.lng();
+    document.getElementById('shipping_moova_lat').value = location.lat();
+    document.getElementById('shipping_moova_lng').value = location.lng();
+    marker.setPosition(location);
+}
+
+class Place{
+  constructor(type, map, marker, autocomplete){
+    this.type = type;
+    this.marker = marker;
+    this.map = map;
+    this.autocomplete = autocomplete
+  }
+
+  changed(){
+    if (this.type === 'billing' && document.getElementById('ship-to-different-address-checkbox').checked) {
+      return
+    }
+    this.marker.setVisible(false);
+    let place = this.autocomplete.getPlace();
     if (!place.geometry) {
       window.alert("No details available for input: '" + place.name + "'");
       return;
@@ -31,14 +65,14 @@ function initMap() {
 
     // If the place has a geometry, then present it on a map.
     if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
+      this.map.fitBounds(place.geometry.viewport);
     } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17); // Why 17? Because it looks good.
+      this.map.setCenter(place.geometry.location);
+      this.map.setZoom(17); // Why 17? Because it looks good.
     }
 
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
+    this.marker.setPosition(place.geometry.location);
+    this.marker.setVisible(true);
     let address = "";
 
     if (place.address_components) {
@@ -58,20 +92,6 @@ function initMap() {
     infowindowContent.children["place-name"].textContent = place.name;
     infowindowContent.children["place-address"].textContent = 'address';
     infowindow.open(map, marker);
-  });
-
-  google.maps.event.addListener(map, 'click', function(event) {
-   placeMarker(event.latLng,marker);
-});
-  
-  function placeMarker(location, marker) {
-    document.getElementById('billing_moova_lat').value = location.lat();
-    document.getElementById('billing_moova_lng').value = location.lng();
-    document.getElementById('shipping_moova_lat').value = location.lat();
-    document.getElementById('shipping_moova_lng').value = location.lng();
-
-    marker.setPosition(location);
   }
+  
 }
-
-//Listen change on ship-to-different-address-checkbox
