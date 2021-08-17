e05c30c8b564e04b16125b2542de516ab510c948
axel.candia <axelcandia2609@gmail.com>
2021-08-17 09:31:29 -0300

    Translate

--- Orders/TrackingShortcode.php
+++ Orders/TrackingShortcode.php
@@ -16,11 +16,11 @@ class TrackingShortcode
      */
     public static function output()
     {
-        $content = '<h2 class="moova-tracking-form-title">' . __('Shipment number', 'wc-moova') . '</h2>
+        $content = '<h2 class="moova-tracking-form-title">' . __('Numero de seguimiento', 'wc-moova') . '</h2>
         <form method="get" class="moova-tracking-form">
         <input type="text" name="moova_tracking_id" style="width:40%" class="moova-tracking-form-field"><br>
         <br />
-        <input name="submit_button" type="submit"  value="' . __('Track shipment', 'wc-moova') . '"  id="update_button"  class="moova-tracking-form-submit update_button" style="cursor: pointer;background-color: #4fa0ff;border: 1px solid #4fa0ff;color: white;padding: 5px 10px;display: inline-block;border-radius: 4px;font-weight: 600;margin-bottom: 10px;text-align: center;"/>
+        <input name="submit_button" type="submit"  value="' . __('Rastree su envio', 'wc-moova') . '"  id="update_button"  class="moova-tracking-form-submit update_button" style="cursor: pointer;background-color: #4fa0ff;border: 1px solid #4fa0ff;color: white;padding: 5px 10px;display: inline-block;border-radius: 4px;font-weight: 600;margin-bottom: 10px;text-align: center;"/>
         </form>';
         if (empty($_GET['moova_tracking_id'])) {
             return $content;
@@ -30,14 +30,14 @@ class TrackingShortcode
         $moovaSdk = new MoovaSdk();
         $tracking_statuses = $moovaSdk->get_tracking($moova_id);
         if ($tracking_statuses === false) {
-            $content .= '<h3  class="moova-tracking-error">' . __('There was an error, please try again', 'wc-moova') . '</h3>';
+            $content .= '<h3  class="moova-tracking-error">' . __('Hubo un error, por favor intente nuevamente', 'wc-moova') . '</h3>';
         } else {
             if (!empty($tracking_statuses)) {
-                $content .= '<h3>' . __('Shipment number', 'wc-moova') . ': ' . $moova_id . '</h3>';
+                $content .= '<h3>' . __('Numero de seguimiento', 'wc-moova') . ': ' . $moova_id . '</h3>';
                 $content .= '<table>';
                 $content .= '<tr>';
-                $content .= '<th width="30%">' . __('Date', 'wc-moova') . '</th>';
-                $content .= '<th width="70%">' . __('Status', 'wc-moova') . '</th>';
+                $content .= '<th width="30%">' . __('Fecha', 'wc-moova') . '</th>';
+                $content .= '<th width="70%">' . __('Estado', 'wc-moova') . '</th>';
                 $content .= '</tr>';
                 foreach ($tracking_statuses as $tracking_status) {
                     $content .= '<tr>';
@@ -47,7 +47,7 @@ class TrackingShortcode
                 }
                 $content .= '</table>';
             } else {
-                $content .= '<h2>' . ('Shipment without status') . '</h2>';
+                $content .= '<h2>' . ('envio sin estado') . '</h2>';
             }
         }
         return $content;
