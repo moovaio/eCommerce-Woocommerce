50f4040622020cc5f7512a5009d4f3b4cb451100
axel.candia <axelcandia2609@gmail.com>
2024-10-17 12:39:38 +0200

    6.0

--- woo-moova.php
+++ woo-moova.php
@@ -5,7 +5,7 @@ use Moova\Helper\Helper;
 /**
  * Plugin Name: Moova for WooCommerce
  * Description: Integration between Moova and WooCommerce
- * Version: 5.9
+ * Version: 6.0
  * Requires PHP: 7.0
  * Author: Moova.io
  * Author URI: https://moova.io/
